1d13baa365b1bff40fd63949dcd3ea7ac0fd9c3b
Bruno Freschi <bruno.freschi2@gmail.com>
2022-06-20 21:30:43 -0300

    BotStart

--- autoPonto/Program.cs
+++ autoPonto/Program.cs
@@ -1,4 +1,29 @@
-﻿
+﻿using OpenQA.Selenium.Chrome;
 
+string url = "https://web.whatsapp.com/";
+
+List<string> contacts = new List<string>()
+{
+    "Teste bot"
+};
+
+ChromeDriver driver = new ChromeDriver();
+
+driver.Navigate().GoToUrl(url);
+driver.Manage().Window.Maximize();
+
+Thread.Sleep(5000);
+
+foreach (var contact in contacts)
+{
+    //<div title="Caixa de texto de pesquisa" role="textbox" class="_13NKt copyable-text selectable-text" contenteditable="true" data-tab="3" dir="ltr"></div>
+    var search = driver.FindElementByClassName("_13NKt");
+    search.SendKeys(contact);
+
+    search = driver.FindElementByXPath($"//spam[@title='{contact}']");
+    search.Click();
+
+
+}
 
 Console.WriteLine("Hello, World!");
